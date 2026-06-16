17d3896c8e5d1128f6160e9c1cc1eac4b2ed686e
adithyakp2004 <45890377+adithyakp2004@users.noreply.github.com>
2026-06-16 17:08:05 +0530

    Update and rename 7th Program (Regression Analysis) to 7th Program (Regression Analysis).py

---
+++ 7th Program (Regression Analysis).py
@@ -0,0 +1,38 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+import numpy as np
+
+data = pd.read_csv("data.csv")
+
+X = data[['Hours']]
+y = data['Marks']
+
+model = LinearRegression()
+model.fit(X, y)
+
+pred = model.predict(X)
+
+plt.scatter(X, y, label='Data Points')
+plt.plot(X, pred, 'r', label='Regression Line')
+plt.title("Regression Line")
+plt.xlabel("Hours")
+plt.ylabel("Marks")
+plt.legend()
+plt.show()
+
+residuals = y - pred
+
+plt.scatter(pred, residuals, label='Residuals')
+plt.axhline(y=0, color='r', label='Zero Error Line')
+plt.title("Residual Plot")
+plt.xlabel("Predicted Marks")
+plt.ylabel("Residuals")
+plt.legend()
+plt.show()
+
+print("MAE =", mean_absolute_error(y, pred))
+print("MSE =", mean_squared_error(y, pred))
+print("RMSE =", np.sqrt(mean_squared_error(y, pred)))
+print("R2 Score =", r2_score(y, pred))
