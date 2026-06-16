54c6a0140ac4adb587de92b8fcfdcbf04f96044d
adithyakp2004 <45890377+adithyakp2004@users.noreply.github.com>
2026-06-16 17:53:12 +0530

    Update 2nd program(Candidate Elimination Algorithm.py

--- 2nd program(Candidate Elimination Algorithm.py
+++ 2nd program(Candidate Elimination Algorithm.py
@@ -2,18 +2,18 @@ import pandas as pd
 
 data = pd.read_csv("dataset1.csv")
 
-features = data[['Weather','Temp']].values
+features = data[['Weather', 'Temp']].values
 labels = data['Result'].values
 
-specific = features[labels=="Yes"][0]
+specific = features[labels == "Yes"][0]
 
-for f in features[labels=="Yes"]:
-specific = [specific[i] if specific[i]==f[i] else "?"
-for i in range(len(f))]
+for f in features[labels == "Yes"]:
+    specific = [specific[i] if specific[i] == f[i] else "?"
+                for i in range(len(f))]
 
 print("Specific Hypothesis:", specific)
 
 print("General Hypothesis:")
-for f in features[labels=="No"]:
-print([specific[i] if f[i]!=specific[i] else "?"
-for i in range(len(f))])
+for f in features[labels == "No"]:
+    print([specific[i] if f[i] != specific[i] else "?"
+           for i in range(len(f))])
